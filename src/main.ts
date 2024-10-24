5a88c39cfebf1a5d3f7312328fab7b76402018c9
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-24 09:54:41 +0200

    Add SDK + Debugging

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,23 @@ import PrimeVue from "primevue/config";
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css';
 
+// Telegram
+import { init } from '@telegram-apps/sdk-vue';
+// Verify Telegram environment is available
+function isTelegramEnvironment() {
+    return typeof window.Telegram !== 'undefined' && window.Telegram.WebApp;
+}
+// Init SDK only if it's a Telegram environment
+if (isTelegramEnvironment()) {
+    init();
+} else {
+    console.warn('App is not running inside Telegram. Skipping Telegram SDK initialization.');
+}
+
+// Eruda - Debugging
+import eruda from 'eruda';
+eruda.init();
+
 import App from './App.vue';
 import router from './router';
 
