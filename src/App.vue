6ef1c126eb1f6b9c09788e389793b7579d9527a6
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-24 10:02:36 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/app/NavBar.vue';
+// import { watchEffect, onMounted, onUnmounted } from 'vue';
+
+// import { backButton, useSignal } from '@telegram-apps/sdk-vue';
+// const isVisible = useSignal(backButton.isVisible);
+
+// watchEffect(() => {
+//   console.log('The button is', isVisible.value ? 'visible' : 'invisible');
+// });
+
+// onMounted(() => {
+//   backButton.show();
+// });
+
+// onUnmounted(() => {
+//   backButton.hide();
+// });
+import { useLaunchParams } from '@telegram-apps/sdk-vue';
+const lp = useLaunchParams();
 </script>
 
 <template>
   <header>
     <NavBar />
   </header>
+
+  <div>Start param: {{ lp.startParam }}</div>
 
   <main class="px-4 mt-6">
     <RouterView />
